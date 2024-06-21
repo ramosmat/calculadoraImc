4abd540b9d319ea07763cf5528c848c99824f538
Matheus <matheusramosbi.13@gmail.com>
2024-06-20 22:23:12 -0300

    prevent input errors and refactored table

--- src/Dados.jsx
+++ src/Dados.jsx
@@ -1,32 +1,62 @@
 import React from 'react';
 
 const Dados = ({ dados }) => {
-  if (dados == null) {
-    return (
-      <div className="min-h-[85px] grid max-[400px]:min-h-[60px]">
-        <p className="mx-auto text-center text-xl text-[#ABABAB] max-w-[25ch] place-self-center max-[400px]:text-lg">
-          Saiba agora se está no seu peso ideal!
-        </p>
-      </div>
-    );
-  } else {
-    return (
-      <div className="min-h-[85px] grid max-[400px]:min-h-[60px]">
-        <div className="border-b border-[#E85B81] grid grid-cols-4 text-center text-[#505050] font-semibold text-lg max-[400px]:text-sm items-center">
-          <p>Peso</p>
-          <p>Altura</p>
-          <p>IMC</p>
-          <p>Resultado</p>
+  //Transformar os ifs abaixo em switch
+  switch (true) {
+    case dados == null:
+      return (
+        <div className="min-h-[85px] grid max-[400px]:min-h-[60px]">
+          <p className="mx-auto text-center text-xl text-[#ABABAB] max-w-[25ch] place-self-center max-[400px]:text-lg">
+            Saiba agora se está no seu peso ideal!
+          </p>
         </div>
+      );
 
-        <div className="grid grid-cols-4 items-center text-center text-lg max-[400px]:text-sm">
-          <p>{dados.peso}</p>
-          <p>{dados.altura}</p>
-          <p>{dados.imc}</p>
-          <p>{dados.resultado}</p>
+    case dados.peso > 150:
+      return (
+        <div className="min-h-[85px] grid max-[400px]:min-h-[60px]">
+          <p className="mx-auto text-center text-xl text-[#000000] max-w-[25ch] place-self-center max-[400px]:text-lg">
+            Você está muito pesado, procure um médico urgente!
+          </p>
         </div>
-      </div>
-    );
+      );
+
+    case dados.altura > 230:
+      return (
+        <div className="min-h-[85px] grid max-[400px]:min-h-[60px]">
+          <p className="mx-auto text-center text-xl text-[#000000] max-w-[25ch] place-self-center max-[400px]:text-lg">
+            Esta altura não condiz com a realidade.
+          </p>
+        </div>
+      );
+
+    case dados.altura < 0 || dados.peso < 0:
+      return (
+        <div className="min-h-[85px] grid max-[400px]:min-h-[60px]">
+          <p className="mx-auto text-center text-xl text-[#000000] max-w-[25ch] place-self-center max-[400px]:text-lg">
+            Insira valores válidos.
+          </p>
+        </div>
+      );
+
+    default:
+      return (
+        <div className="min-h-[85px] grid max-[400px]:min-h-[60px]">
+          <div className="border-b border-[#E85B81] grid grid-cols-4 text-center text-[#505050] font-semibold text-lg max-[400px]:text-sm items-center">
+            <p>Peso</p>
+            <p>Altura</p>
+            <p>IMC</p>
+            <p>Resultado</p>
+          </div>
+
+          <div className="grid grid-cols-4 items-center text-center text-lg max-[400px]:text-sm">
+            <p>{dados.peso}</p>
+            <p>{dados.altura}</p>
+            <p>{dados.imc}</p>
+            <p>{dados.resultado}</p>
+          </div>
+        </div>
+      );
   }
 };
 
--- src/Tabela.jsx
+++ src/Tabela.jsx
@@ -2,26 +2,44 @@ import React from 'react';
 
 const Tabela = () => {
   return (
-    <div className="grid grid-cols-2">
-      <ul className="flex flex-col justify-center text-start max-[400px]:text-sm">
+    <div className="flex flex-col">
+      <ul className="flex gap-3 justify-between max-[400px]:text-sm">
         <p className="text-[#E85B81] font-bold mb-1">IMC</p>
-        <li>Menos de 17</li>
-        <li>Entre 17 e 18,49</li>
-        <li>Entre 18,5 e 24,99</li>
-        <li>Entre 25 e 29,99</li>
-        <li>Entre 30 e 34,99</li>
-        <li>Entre 35 e 39,99</li>
-        <li>Acima de 40</li>
+        <p className="text-[#E85B81] font-bold mb-1">Classificação</p>
       </ul>
 
-      <ul className="flex flex-col justify-center text-end max-[400px]:text-sm">
-        <p className="text-[#E85B81] font-bold mb-1">Classificação</p>
+      <ul className="flex gap-3 justify-between max-[400px]:text-sm">
+        <li>Menos de 17</li>
         <li>Muito abaixo do peso</li>
+      </ul>
+
+      <ul className="flex gap-3 justify-between max-[400px]:text-sm">
+        <li>Entre 17 e 18,49</li>
         <li>Abaixo do peso</li>
+      </ul>
+
+      <ul className="flex gap-3 justify-between max-[400px]:text-sm">
+        <li>Entre 18,5 e 24,99</li>
         <li>Peso normal</li>
+      </ul>
+
+      <ul className="flex gap-3 justify-between max-[400px]:text-sm">
+        <li>Entre 25 e 29,99</li>
         <li>Acima do peso</li>
+      </ul>
+
+      <ul className="flex gap-3 justify-between max-[400px]:text-sm">
+        <li>Entre 30 e 34,99</li>
         <li>Obesidade I</li>
+      </ul>
+
+      <ul className="flex gap-3 justify-between max-[400px]:text-sm">
+        <li>Entre 35 e 39,99</li>
         <li>Obesidade II</li>
+      </ul>
+
+      <ul className="flex gap-3 justify-between items-center max-[400px]:text-sm">
+        <li>Acima de 40</li>
         <li>Obesidade III</li>
       </ul>
     </div>
